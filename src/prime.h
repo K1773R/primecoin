04460cc11faa1941ef14afef7c4ed9bdd0d1b8c0
K1773R <K1773R@darkgamex.ch>
2013-10-22 20:24:22 +0200

    make primeminer rpool.net compatible (ie, submitting 6' shares too)

--- src/prime.h
+++ src/prime.h
@@ -15,7 +15,7 @@
 /* POOL ADDON */
 /**************/
 
-static const unsigned int POOL_SHARE_MINIMUM = 7;
+static const unsigned int POOL_SHARE_MINIMUM = 6;
 extern size_t thread_num_max;
 
 class CBlockProvider {
